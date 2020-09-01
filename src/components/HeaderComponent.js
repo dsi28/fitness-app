fceb9fbbc4af643aee3d3b8700ba8d159cfdc451
david manly <manly.david9@gmail.com>
2020-09-01 12:22:56 -0500

    added footer, header and home component.

---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+
+
+
+
+class Header extends Component {
+	render() {
+		return <h1> HeaderComponent</h1>;
+	}
+}
+
+export default Header;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,14 +1,23 @@
 import React, {Component} from 'react';
 
 import Login from './LoginComponent';
+import Home from './HomeComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
+
 
 class Main extends Component{
     render(){
         return(
             <>
+                <Header/>
                 <Login/>
+                <Home/>
+                <Footer/>
             </>
         )
     }
 }
+
+
 export default Main;
