a74c95ad08caf56faed90e54a000df0b293c5061
david manly <manly.david9@gmail.com>
2020-09-01 15:38:14 -0500

    Saved router files

--- src/shared/router.js
+++ src/shared/router.js
@@ -3,11 +3,13 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from '../components/HomeComponent';
 import Login from '../components/LoginComponent';
+import CreateWorkout from '../components/CreateWorkoutComponent';
 
 export default (
     <Switch>
         <Route exact path='/' component={Home} />
         <Route path='/login' component={Login} />
+        <Route path='/createworkout' component={CreateWorkout} />
         <Redirect to='/'/>
     </Switch>
 )
