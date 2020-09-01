bd58781d2c29e0dd4c557fd4fca21aacec5c4121
David <dsi28@hotmail.com>
2020-09-01 15:01:29 -0500

    update home component to route user to another component on button click

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,8 +1,19 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 class Home extends Component {
+
+	handleCreateWorkoutClick = () => {
+		console.log('test');
+	}
 	render() {
-		return <h1> HomeComponent </h1>;
+		return (
+			<>
+				<Link to={`/login`}>
+					<button onClick={() => this.handleCreateWorkoutClick()} >Create Workout</button>
+				</Link>
+			</>
+		);
 	}
 }
 export default Home;
